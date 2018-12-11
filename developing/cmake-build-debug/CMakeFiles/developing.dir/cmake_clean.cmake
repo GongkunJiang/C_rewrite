923eae4d0427bf818c334447a46b3bc151027e27
GongkunJiang <jgk19950903@gmail.com>
2018-12-11 12:52:21 +0800

    clion

---
+++ developing/cmake-build-debug/CMakeFiles/developing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/developing.dir/main.c.obj"
+  "developing.pdb"
+  "developing.exe"
+  "developing.exe.manifest"
+  "libdeveloping.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/developing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
